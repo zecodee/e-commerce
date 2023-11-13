14c101fcd3c4a321576a9413d35df5b344faf8db
zeycodee <148941016+zecodee@users.noreply.github.com>
2023-11-13 14:09:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,29 +24,32 @@ if (document.readyState == "loading") {
 
 // making function
 function ready() {
-    var removeCartButtondss = document.getElementsByClassName("cart-remove");
-    for (var i = 0; i < removeCartButtondss.length; i++) {
-        var button = removeCartButtons[i];
+    let removeCartButtons = document.getElementsByClassName("cart-remove");
+    for (let i = 0; i < removeCartButtons.length; i++) {
+        let button = removeCartButtons[i];
         button.addEventListener("click", removeCartItem);
     }
-    var quantityInputs = document.getElementsByClassName('cart-quantity');
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i];
+
+    let quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i];
         input.addEventListener("change", quantityChanged);
     }
+
     // add to cart 
-    var addCart = document.getElementsByClassName('add-cart');
-    for (var i = 0; i < addCart.length; i++) {
-        var button = addCart[i];
-        button.addEventListener("click", addCartClicked)
+    let addCartButtons = document.getElementsByClassName('add-cart');
+    for (let i = 0; i < addCartButtons.length; i++) {
+        let button = addCartButtons[i];
+        button.addEventListener("click", addCartClicked);
     }
+
     // buy button work
     document.getElementsByClassName("btn-buy")[0].addEventListener("click", buyButtonClicked);
 }
 
 function buyButtonClicked() {
-    var cartContent = document.getElementsByClassName("cart-content")[0];
-    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    let cartContent = document.getElementsByClassName("cart-content")[0];
+    let cartBoxes = cartContent.getElementsByClassName("cart-box");
 
     // Periksa apakah keranjang kosong
     if (cartBoxes.length === 0) {
@@ -61,22 +64,22 @@ function buyButtonClicked() {
         // Menutup cart
         cart.classList.remove("active");
 
-        var openPopup = document.getElementById("popup");
+        let openPopup = document.getElementById("popup");
         openPopup.classList.add("open-popup");
 
         // Membuat pesan yang berisi isi dari cartBoxContent
-        var message = "My Order:\n";
-        for (var i = 0; i < cartBoxes.length; i++) {
-            var cartBox = cartBoxes[i];
-            var title = cartBox.querySelector(".cart-product-title").innerText;
-            var price = cartBox.querySelector(".cart-price").innerText;
-            var quantity = cartBox.querySelector(".cart-quantity").value;
+        let message = "My Order:\n";
+        for (let i = 0; i < cartBoxes.length; i++) {
+            let cartBox = cartBoxes[i];
+            let title = cartBox.querySelector(".cart-product-title").innerText;
+            let price = cartBox.querySelector(".cart-price").innerText;
+            let quantity = cartBox.querySelector(".cart-quantity").value;
             message += `${title}\nPrice: ${price}\nQuantity: ${quantity}\n\n`;
         }
 
         // Menghitung total
-        var totalElement = document.getElementsByClassName("total-price")[0];
-        var total = totalElement.innerText;
+        let totalElement = document.getElementsByClassName("total-price")[0];
+        let total = totalElement.innerText;
 
         // Menambahkan total ke pesan
         message += `Total: ${total}`;
@@ -98,6 +101,8 @@ function buyButtonClicked() {
             }
             openPopup.classList.remove("open-popup");
             updatetotal();
+            saveCartToLocalStorage();
+            cartItem.remove();
         }, 3000); // Penundaan 3000 milidetik (3 detik)
     }
 }
@@ -106,14 +111,22 @@ function buyButtonClicked() {
 
 // remove function
 function removeCartItem(event) {
-    var buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
+    let buttonClicked = event.target;
+    let cartItem = buttonClicked.parentElement;
+
+    // Remove the item from the cart
+    cartItem.remove();
+
+    // Update the total in the cart
     updatetotal();
+
+    // Save the updated cart to local storage
+    saveCartToLocalStorage();
 }
 
 // change quantity
 function quantityChanged(event) {
-    var input = event.target;
+    let input = event.target;
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1;
     }
@@ -122,42 +135,35 @@ function quantityChanged(event) {
 
 // add to cart
 function addCartClicked(event) {
-    var button = event.target;
-    var shopProducts = button.parentElement;
-    var title = shopProducts.querySelector(".product-title").innerText;
-    var price = shopProducts.querySelector(".price").innerText;
-    var productImg = shopProducts.querySelector(".product-img").src;
+    let button = event.target;
+    let shopProducts = button.parentElement;
+    let title = shopProducts.querySelector(".product-title").innerText;
+    let price = shopProducts.querySelector(".price").innerText;
+    let productImg = shopProducts.querySelector(".product-img").src;
 
-    // Periksa apakah item dengan title yang sama sudah ada di keranjang
-    var cartItems = document.getElementsByClassName("cart-content")[0];
-    var cartItemsBoxes = cartItems.getElementsByClassName("cart-box");
-    for (var i = 0; i < cartItemsBoxes.length; i++) {
-        var cartItem = cartItemsBoxes[i];
-        var cartTitle = cartItem.querySelector(".cart-product-title").innerText;
-        if (cartTitle === title) {
-            alert("this item is already in the cart");
-            return; 
-        }
-    }
-
+    // nambah product to the cart
     addProductToCart(title, price, productImg);
+
+    // mengupdate total in the cart
     updatetotal();
+
+    // ngesave updated cart to local storage
+    saveCartToLocalStorage();
 }
 
-
 function addProductToCart(title, price, productImg) {
-    var cartShopBox = document.createElement("div");
+    let cartShopBox = document.createElement("div");
     cartShopBox.classList.add("cart-box");
-    var cartItems = document.getElementsByClassName("cart-content")[0];
-    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
-    for (var i = 0; i < cartItemsNames.length; i++) {
+    let cartItems = document.getElementsByClassName("cart-content")[0];
+    let cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (let i = 0; i < cartItemsNames.length; i++) {
         if (cartItemsNames[i].innerText == title) {
             alert("You add this item");
             return;
         }
     }
 
-    var cartBoxContent = `
+    let cartBoxContent = `
         <img src="${productImg}" alt="#" class="cart-img">
         <div class="detail-box">
             <div class="cart-product-title">${title}</div>
@@ -175,14 +181,14 @@ function addProductToCart(title, price, productImg) {
 
 // update total
 function updatetotal() {
-    var cartBoxes = document.getElementsByClassName('cart-box');
-    var total = 0;
-    for (var i = 0; i < cartBoxes.length; i++) {
-        var cartBox = cartBoxes[i];
-        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
-        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-        var price = parseFloat(priceElement.innerText.replace("Rp. ", "").replace(".", "").replace(",", ""));
-        var quantity = parseInt(quantityElement.value);
+    let cartBoxes = document.getElementsByClassName('cart-box');
+    let total = 0;
+    for (let i = 0; i < cartBoxes.length; i++) {
+        let cartBox = cartBoxes[i];
+        let priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        let price = parseFloat(priceElement.innerText.replace("Rp. ", "").replace(".", "").replace(",", ""));
+        let quantity = parseInt(quantityElement.value);
         total += price * quantity;
     }
     // Format total sebagai mata uang Indonesia dengan tiga angka sebelum titik
@@ -271,3 +277,52 @@ menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 // End navbar burbger
+
+// Local Storage
+
+// Function to get items to local storage
+function getCartItems() {
+    return JSON.parse(localStorage.getItem('save-cart')) || [];
+}
+
+// Function to save items to local storage
+function saveCartItems(items) {
+    localStorage.setItem('save-cart', JSON.stringify(items));
+}
+
+function saveCartToLocalStorage() {
+    let cartItems = [];
+    let cartBoxes = document.getElementsByClassName('cart-box');
+    for (let i = 0; i < cartBoxes.length; i++) {
+        let cartBox = cartBoxes[i];
+        let title = cartBox.querySelector(".cart-product-title").innerText;
+        let price = cartBox.querySelector(".cart-price").innerText;
+        let quantity = cartBox.querySelector(".cart-quantity").value;
+        let productImg = cartBox.querySelector(".cart-img").src;
+
+        let cartItem = {
+            title: title,
+            price: price,
+            quantity: quantity,
+            productImg: productImg
+        };
+        cartItems.push(cartItem);
+    }
+
+    // simpan cart data to local storage
+    saveCartItems(cartItems);
+}
+
+function loadCartFromLocalStorage() {
+    let cartItems = getCartItems();
+
+    //  menambah setiap item dari penyimpanan lokal ke keranjang
+    for (let i = 0; i < cartItems.length; i++) {
+        let cartItem = cartItems[i];
+        addProductToCart(cartItem.title, cartItem.price, cartItem.productImg);
+        updatetotal();
+    }
+}
+
+// manggil function load cart saat mengload halaman website
+loadCartFromLocalStorage();
